9b6e56af2b664b13d4475c83ff46f4d88f4569c3
Matt Hyde <matt.paul.hyde@outlook.com>
2017-07-21 14:15:52 +0100

    Added customer api

---
+++ Finning.Web/Finning.Web/Builders/CustomerViewModelBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Finning.Web.Models;
+
+namespace Finning.Web.Builders
+{
+    public class CustomerViewModelBuilder
+    {
+        public CustomerViewModel Build(Customer customer)
+        {
+            var viewModel = new CustomerViewModel
+            {
+                AccountNumber = customer.AccountNumber,
+                Name = customer.Name
+            };
+            return viewModel;
+        }
+
+        public List<CustomerViewModel> Build(IList<Customer> customers)
+        {
+            return customers.Select(Build).ToList();
+        }
+    }
+}
---
+++ Finning.Web/Finning.Web/Controllers/Api/CustomersController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Finning.Web.Builders;
+using Finning.Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Finning.Web.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/Customers")]
+    public class CustomersController : Controller
+    {
+        public List<Customer> Customers { get; set; }
+
+        public CustomersController()
+        {
+            Customers = new List<Customer>
+            {
+                new Customer
+                {
+                    Name = "Special Customer",
+                    AccountNumber = "ABC123",
+                    Address1 = "123 Special Road",
+                    Address2 = "London",
+                    PostCode = "W1 1AA",
+                    CreditLimit = 1000000
+                }
+            };
+        }
+
+        [HttpGet]
+        [Route("")]
+        [ProducesResponseType(typeof(List<CustomerViewModel>), StatusCodes.Status200OK))]
+        public IActionResult Get()
+        {
+            var builder = new CustomerViewModelBuilder();
+            var result = builder.Build(Customers);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("{accountNumber}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(Customer), StatusCodes.Status200OK)]
+        public IActionResult Get(string accountNumber)
+        {
+            var result = Customers.SingleOrDefault(customer => customer.AccountNumber == accountNumber);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+    }
+}
