eb3f75942ef66d59aca444ae29c3edd3377e2af8
Matt Hyde <matt.paul.hyde@outlook.com>
2017-07-21 16:50:57 +0100

    Added location starter code

---
+++ Finning.Web/Finning.Web/Models/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Finning.Web.Models
+{
+    public class Location
+    {
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Logitude { get; set; }
+        public int NumberOfMachines { get; set; }
+    }
+}
